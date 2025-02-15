64d60db836efee4f8b6d7123c7e5d216951268f7
choi <gg78700@gmail.com>
2025-02-15 21:04:28 +0900

    tm[

--- mongddang_test.py
+++ mongddang_test.py
@@ -57,7 +57,8 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument('--window-size=1920,1080')
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-
+service=ChromeService(ChromeDriverManager().install())
+print(service.path)
 mongddang_list= []
 start_time = time.time()
 driver = webdriver.Chrome(options=options)
